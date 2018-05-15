e1e40b99527ea050c9840f66038a128fe16890c9
charles-henri poniard <c.poniard@gmail.com>
2018-05-15 15:57:10 +0200

    status reservation accepted? refused?

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -10,10 +10,29 @@ class ReservationsController < ApplicationController
       render "bikes/:id/show"
     end
   end
+
   def index
     @reservations = Reservation.where(user_id: current_user)
   end
+
+  def accepted?
+    @reservation = Reservation.find(params[:id])
+    @reservation.status = true
+    @reservation.save
+    redirect_to reservations_path
+  end
+
+  def refused?
+    @reservation = Reservation.find(params[:id])
+    @reservation.status = false
+    @reservation.save
+    redirect_to reservations_path
+  end
+
+
   private
+
+
   def reservation_params
     params.require(:reservation).permit(:user, :bike, :date, :status, :rating)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   root to: "bikes#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create]
+  patch 'reservations/:id/accepted', to: 'reservations#accepted?', as: 'accepted'
+  patch 'reservations/:id/refused', to: 'reservations#refused?', as: 'refused'
+
 
   resources :bikes, only: [:index, :new, :create, :show] do
     resources :reservations, only: :create, shallow: true
   end
-
   resources :reservations, only: :index
 end
