4fd5bc80045e335173c8e624cf493c9865f18ca3
Romain Grossard <rgrossard@gmail.com>
2018-05-15 12:33:08 +0200

    add application_helper for bike in index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
 
-
+  def default_photo(record)
+    if record.photo.present?
+      cl_image_tag record.photo, height: 300, width: 400, crop: :fill
+    else
+      image_tag("photo_uniq.png", height: 300, width: 400, crop: :fill )
+    end
+  end
 
 end
