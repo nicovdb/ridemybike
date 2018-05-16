d8638a497c4d0442002ecb8f5b45e043a4b6d800
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2018-05-16 11:38:33 +0200

    rajouté address et coordonnées aux users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_16_091944) do
+ActiveRecord::Schema.define(version: 2018_05_16_093207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,9 @@ ActiveRecord::Schema.define(version: 2018_05_16_091944) do
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
     t.string "photo"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
