b059bd4ffef96fa2d87a48e8983a28150e60f22a
charles-henri poniard <c.poniard@gmail.com>
2018-05-16 15:33:17 +0200

    search in progress

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -6,7 +6,10 @@ class BikesController < ApplicationController
 
     @bikes = policy_scope(Bike)
     if params.dig(:search, :category).present?
-      @bikes = @bikes.where(address: set_params_search[:address], category: set_params_search[:category])
+      # @bikes = @bikes.where(address: set_params_search[:address], category: set_params_search[:category]).order(:created_at)
+      @bikes = @bikes.where("address ILIKE ?", "%#{params[:search][:address]}%")
+        #category: params[:search][:category]).order(:created_at)
+
     else
       @bikes = policy_scope(Bike)
     end
@@ -33,10 +36,6 @@ class BikesController < ApplicationController
     end
   end
 
-  def search
-
-  end
-
   private
 
   def set_params
@@ -44,9 +43,9 @@ class BikesController < ApplicationController
 
   end
 
-  def set_params_search
-    params.require(:search).permit(:address, :category)
-  end
+  # def set_params_search
+  #   params.require(:search).permit(:address, :category)
+  # end
 
   def set_bike
     @bike = Bike.find(params[:id])
